5ebdf54631ce2b8e85e6f88d0c47d49e1ce2dc8c
antilex <antilex@users.noreply.github.com>
2018-05-09 09:24:55 +0200

    very rough outline
    i drew some rectangles with a mock array and x,y values which we need to get from centering image on board

--- GOF_assignment/src/application/GOLCanvas.java
+++ GOF_assignment/src/application/GOLCanvas.java
@@ -28,8 +28,30 @@ public class GOLCanvas extends Canvas {
         // Alex: implement drawing on the canvas according to the layout of the board
         //******************************************************************
 		
-		clear();
 		
+
+		//sample array
+		char[] array = {'+', '.', '+','+','.','.','.','+'};
+
+		//sample initial x, y values (to be implemented when centering image on board)
+		int XCOORDINATE = 80;
+		int YCOORDINATE = 90; 
+
+		//for loop to traverse array 
+		for (int i = 0; (i < array.length); i++) {
+			if (array[i] == GOLBoard.REPRESENTATIONLIVECELL) {
+				//draw rectangles on canvas 
+				//requires x,y, width and height 
+				gc.setStroke(Color.BLACK);
+				gc.strokeRect(XCOORDINATE, YCOORDINATE, CELLSIZE, CELLSIZE);
+				XCOORDINATE += 7;
+				//if cell is dead display empty space 
+			} else if (array[i] == GOLBoard.REPRESENTATIONDEADCELL) {
+				gc.clearRect(XCOORDINATE, YCOORDINATE, CELLSIZE, CELLSIZE);
+				XCOORDINATE += 7;
+			}
+		}
+
 	}
 
 	public void clear() {
